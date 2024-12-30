212c3c5554c37d47d71410ef060bb144f4d5d376
Ситниковы <sitnikovmatvey2008@gmail.com>
2024-12-30 20:16:05 +0300

    init: past

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -21,11 +21,12 @@
  *    getIntervalArray(3, 3) => [ 3 ]
  */
 function getIntervalArray(start, end) {
-  const arr = [];
-  for (let i = start; i <= end; i += 1) {
-    arr.push(i);
-  }
-  return arr;
+  return Array.from(
+    {
+      length: end - start + 1,
+    },
+    (_, i) => start + i
+  );
 }
 
 /**
@@ -41,8 +42,15 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+  const res = [];
+  for (let i = 0; i < maxLength; i += 1) {
+    const value1 = i < arr1.length ? arr1[i] : 0;
+    const value2 = i < arr2.length ? arr2[i] : 0;
+    res.push(value1 + value2);
+  }
+  return res;
 }
 
 /**
@@ -57,8 +65,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -75,8 +83,14 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let noFake = 0;
+  arr.forEach((value) => {
+    if (value === item) {
+      noFake += 1;
+    }
+  }, 0);
+  return noFake;
 }
 
 /**
@@ -105,8 +119,8 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
 }
 
 /**
@@ -123,8 +137,10 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((acc, current) => acc + current, 0);
+  return Math.round((sum / arr.length) * 100) / 100;
 }
 
 /**
@@ -137,8 +153,9 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return 0;
+  return arr.every((item) => arr[0].length === item.length);
 }
 
 /**
@@ -152,8 +169,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((item, index) => item === index);
 }
 
 /**
@@ -167,8 +184,8 @@ function isValueEqualsIndex(/* arr */) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -182,8 +199,8 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 /**
@@ -201,7 +218,7 @@ function getTail(arr, n) {
   if (n === 0) {
     return [];
   }
-  return arr.slice(n);
+  return arr.slice(-n);
 }
 
 /**
@@ -239,8 +256,8 @@ function doubleArray(arr) {
  *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
  *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return arr.join();
 }
 
 /**
@@ -492,16 +509,18 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
 function findLongestIncreasingSubsequence(nums) {
-  if (nums.length === 0) return 0;
-  const dp = Array(nums.length).fill(1);
-  for (let i = 1; i < nums.length; i += 1) {
-    for (let j = 0; j < i; j += 1) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
+  let arr = [];
+  let longestArr = [];
+  nums.forEach((item) => {
+    if (item > arr[item]) {
+      arr.push(item);
     }
-  }
-  return Math.max(...dp);
+    if (item < arr[item]) {
+      longestArr = arr;
+      arr = [];
+    }
+  });
+  return longestArr;
 }
 
 /**
@@ -536,11 +555,12 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
 function shiftArray(arr, n) {
-  const shifts = n % arr.length;
-  for (let i = 0; i < shifts; i += 1) {
-    const shiftsIsReady = arr.shift();
-    arr.push(shiftsIsReady);
+  if (arr.length === 0) return arr;
+  let num = n % arr.length;
+  if (n < 0) {
+    num += arr.length;
   }
+  return arr.slice(-num).concat(arr.slice(0, arr.length - num));
 }
 
 /**
@@ -593,8 +613,19 @@ function sortDigitNamesByNumericOrder(arr) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length === 0) return [];
+  if (arr.length === 1 || arr.length === 2) return arr.reverse();
+  const middleIndex = Math.floor(arr.length / 2);
+  let newArray;
+  if (arr.length % 2 === 0) {
+    newArray = arr.slice(middleIndex).concat(arr.slice(0, middleIndex));
+  } else {
+    newArray = arr
+      .slice(middleIndex + 1)
+      .concat(arr[middleIndex], arr.slice(0, middleIndex));
+  }
+  return newArray;
 }
 
 module.exports = {
